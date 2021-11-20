391f6f021ec50ec0ba687d4c6dfcce53efb82798
Magnus Karström <83014443+mrkarstrom@users.noreply.github.com>
2021-11-20 11:48:40 +0100

    Found and corrected the error of id in js

--- lessons/263_practice_form_events_and_PreventDefault/app.js
+++ lessons/263_practice_form_events_and_PreventDefault/app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#shelterForm");
 const input = document.querySelector("#catName");
-const list = document.querySelector("#list");
+const list = document.querySelector("#cats");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log("Submitted!");
