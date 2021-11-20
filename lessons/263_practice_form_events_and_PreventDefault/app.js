e179c26acb0fb5764aa49c8e6776abc07a538c6d
Magnus Karström <83014443+mrkarstrom@users.noreply.github.com>
2021-11-20 11:46:41 +0100

    Creates and adds the input value to newLI

---
+++ lessons/263_practice_form_events_and_PreventDefault/app.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#list");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("Submitted!");
+    const catName = input.value;
+    const newLI = document.createElement("LI");
+    newLI.innerText=catName;
+    list.append(newLI);
+    input.value = "";
+})
