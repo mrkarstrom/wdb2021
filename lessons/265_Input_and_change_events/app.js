aadadeb4f804e236b3139f5eff71a000073bef89
Magnus Karström <83014443+mrkarstrom@users.noreply.github.com>
2021-11-21 01:27:51 +0100

    This is Input Event!

--- lessons/265_Input_and_change_events/app.js
+++ lessons/265_Input_and_change_events/app.js
@@ -1,5 +1,9 @@
 const input = document.querySelector('input');
 
-input.addEventListener('change', function(e) {
-    console.log('Whatever');
+// input.addEventListener('change', function(e) {
+//     console.log('Whatever');
+// })
+
+input.addEventListener('input', function(e) {
+    console.log('Input event!');
 })
