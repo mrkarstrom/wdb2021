bd99df388e12eae60c9f97170caeb1aeeb4e2605
Magnus Karström <83014443+mrkarstrom@users.noreply.github.com>
2021-11-27 18:30:09 +0100

    Added isGameOver boolean which makes game logic simpler

--- lessons/268_ping_pong_score_keeper/app.js
+++ lessons/268_ping_pong_score_keeper/app.js
@@ -8,20 +8,25 @@ const p2Display = document.querySelector('#p2Display');
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
-
+let isGameOver = false;
 
 p1btn.addEventListener('click', function() {
-    if(p1Score != winningScore) {
+    if(!isGameOver) {
         p1Score += 1;
         p1Display.textContent = p1Score;
+    }
+    if(p1Score === winningScore) {
+        isGameOver=true;
     }
 } );
 
 p2btn.addEventListener('click', function() {
-    if(p2Score != winningScore) {
+    if(!isGameOver) {
         p2Score += 1;
         p2Display.textContent = p2Score;
     }
-    
+    if(p2Score === winningScore) {
+        isGameOver=true;
+    }
 } );
 
