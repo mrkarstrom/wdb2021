507cda31c0593361c92f198e68e99221042c8e74
Magnus Karström <83014443+mrkarstrom@users.noreply.github.com>
2021-11-27 18:27:24 +0100

    Added winningScore to JS

--- lessons/268_ping_pong_score_keeper/app.js
+++ lessons/268_ping_pong_score_keeper/app.js
@@ -4,17 +4,24 @@ const reset = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
+
 let p1Score = 0;
 let p2Score = 0;
+let winningScore = 5;
 
 
 p1btn.addEventListener('click', function() {
-    p1Score += 1;
-    p1Display.textContent = p1Score;
+    if(p1Score != winningScore) {
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+    }
 } );
 
 p2btn.addEventListener('click', function() {
-    p2Score += 1;
-    p2Display.textContent = p2Score;
+    if(p2Score != winningScore) {
+        p2Score += 1;
+        p2Display.textContent = p2Score;
+    }
+    
 } );
 
